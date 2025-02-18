67d6691ab017223e7ddf72b3bdc5eca351296fea
Eyanla Garcia <eygarcia@iu.edu>
2025-02-17 22:04:53 -0500

    N320 Homework 2 assignment 3

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Eyanla Garcia | N320 | 2/17/25
+
+# Project Name: N320 Homework 2 | basic web server
+
+## weekly recap:
+
+- we went over creating web servers using node.js
+- we discussed async/ wait
+- we also went over promises
---
+++ views/app/app.js
@@ -0,0 +1,6 @@
+document.getElementById("sillyButton").addEventListener("click", function () {
+  document.getElementById("sillyPic").style.display = "block";
+  document.getElementById("trickP").style.display = "block";
+});
+
+//when the button is clicked from the html, the image and text are displayed using js
